9ee5ebcce8753dfd3ebbc97f5070c42ab199e1a8
Brandon Manson <mansonbrandon@gmail.com>
2017-03-16 22:14:03 -0400

    initial commit

---
+++ src/main/java/com/brandonmanson/SwaggerGeneratorApplication.java
@@ -0,0 +1,12 @@
+package com.brandonmanson;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwaggerGeneratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwaggerGeneratorApplication.class, args);
+	}
+}
