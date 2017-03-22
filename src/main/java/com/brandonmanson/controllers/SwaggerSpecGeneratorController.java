4e939b7fe4395425d61ca1f8ce8fdebfdc9b8abe
Brandon Manson <brandon@iruleathome.com>
2017-03-21 21:43:31 -0400

    big commit. added swagger generator logic and call in controller. currently shipping for debug

--- src/main/java/com/brandonmanson/controllers/SwaggerSpecGeneratorController.java
+++ src/main/java/com/brandonmanson/controllers/SwaggerSpecGeneratorController.java
@@ -1,6 +1,12 @@
 package com.brandonmanson.controllers;
 
 import com.brandonmanson.models.SlackRequest;
+import com.brandonmanson.services.SwaggerSpecGeneratorService;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ArrayNode;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.SystemEnvironmentPropertySource;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
@@ -12,12 +18,21 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping(value = "/swaggerSpecs")
 public class SwaggerSpecGeneratorController {
 
+    @Autowired
+    SwaggerSpecGeneratorService service;
+
     @RequestMapping(method = RequestMethod.POST, consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
     @ResponseStatus(HttpStatus.OK)
     @ResponseBody
     public String generateSwaggerSpecAndReturnAck(SlackRequest request) {
         String[] swaggerValues = request.generateSwaggerValues();
-        String ackString = "Looks good. We're generating a Swagger spec for: \npath: " + swaggerValues[0] + "\nmethod: " + swaggerValues[1] + "\nnumber of parameters: " + swaggerValues[2];
+        String spec = service.generateSwaggerSpec(swaggerValues);
+
+        String ackString = "Looks good. We're generating a Swagger spec for: \npath: " + swaggerValues[0]
+                + "\nmethod: " + swaggerValues[1]
+                + "\nnumber of parameters: " + swaggerValues[2]
+                + "\njson:"
+                + "\n" + spec;
         return ackString;
     }
 
