bc05356b905cad1c309605ecaa8e43f8fe318d52
Brandon Manson <mansonbrandon@gmail.com>
2017-03-24 07:33:18 -0400

    added task executor to application file

--- src/main/java/com/brandonmanson/SwaggerGeneratorApplication.java
+++ src/main/java/com/brandonmanson/SwaggerGeneratorApplication.java
@@ -2,11 +2,23 @@ package com.brandonmanson;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.task.TaskExecutor;
 import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
 @SpringBootApplication
 @EnableAsync
 public class SwaggerGeneratorApplication {
+
+	@Bean
+	public TaskExecutor taskExecutor() {
+		ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+		executor.setCorePoolSize(5);
+		executor.setMaxPoolSize(10);
+		executor.setQueueCapacity(25);
+		return executor;
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(SwaggerGeneratorApplication.class, args);
