13c096eef3e96f2ed7f7b3ea970219f7cddb5d1e
Brandon Manson <mansonbrandon@gmail.com>
2017-03-28 19:36:21 -0400

    added unique team id check for db

--- src/main/java/com/brandonmanson/controllers/AuthenticationController.java
+++ src/main/java/com/brandonmanson/controllers/AuthenticationController.java
@@ -2,6 +2,7 @@ package com.brandonmanson.controllers;
 
 import com.brandonmanson.models.Team;
 import com.brandonmanson.repositories.TeamRepository;
+import com.brandonmanson.services.RetrieveTeamService;
 import org.json.JSONException;
 import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,6 +15,8 @@ import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
 
 /**
  * Created by brandonmanson on 3/26/17.
@@ -30,8 +33,12 @@ public class AuthenticationController {
     @Autowired
     private TeamRepository teamRepository;
 
+    @Autowired
+    private RetrieveTeamService retrieveTeamService;
+
     @RequestMapping(value = "/redirect", method = RequestMethod.GET)
     public String getApiToken(@RequestParam String code) throws JSONException {
+
         RestTemplate restTemplate = new RestTemplate();
         HttpHeaders headers = new HttpHeaders();
         headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
@@ -50,13 +57,23 @@ public class AuthenticationController {
         String botAccessToken = responseToJson.getJSONObject("bot").get("bot_access_token").toString();
         String botUserId = responseToJson.getJSONObject("bot").get("bot_user_id").toString();
 
-        Team team = new Team();
-        team.setScope(responseToJson.getString("scope"));
-        team.setTeamName(responseToJson.getString("team_name"));
-        team.setTeamId(responseToJson.getString("team_id"));
-        team.setBotAccessToken(botAccessToken);
-        team.setBotUserId(botUserId);
-        teamRepository.save(team);
+        List<Team> teamList = teamRepository.findByTeamId(responseToJson.getString("team_id"));
+
+        if (teamList.size() == 0)
+        {
+            Team team = new Team();
+            team.setScope(responseToJson.getString("scope"));
+            team.setTeamName(responseToJson.getString("team_name"));
+            team.setTeamId(responseToJson.getString("team_id"));
+            team.setBotAccessToken(botAccessToken);
+            team.setBotUserId(botUserId);
+            teamRepository.save(team);
+        } else {
+            Team team = teamList.get(0);
+            team.setBotAccessToken(botAccessToken);
+            team.setBotUserId(botUserId);
+            teamRepository.save(team);
+        }
 
 
         return "Authenticated!";
