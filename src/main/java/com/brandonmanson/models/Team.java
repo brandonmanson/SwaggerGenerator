d1311a04bac4823e970498648332ecd161d0f8f6
Brandon Manson <brandon@iruleathome.com>
2017-03-28 19:36:21 -0400

    added unique team id check for db

--- src/main/java/com/brandonmanson/models/Team.java
+++ src/main/java/com/brandonmanson/models/Team.java
@@ -4,10 +4,7 @@ package com.brandonmanson.models;
  * Created by brandonmanson on 3/27/17.
  */
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 /**
  * Created by brandonmanson on 3/26/17.
@@ -18,9 +15,11 @@ public class Team {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Integer id;
 
+    @Column(name = "teamId", unique = true)
+    private String teamId;
+
     private String scope;
     private String teamName;
-    private String teamId;
     private String botAccessToken;
     private String botUserId;
 
